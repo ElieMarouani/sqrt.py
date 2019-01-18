f4dbf756e8a1f845748b31244399b717d8763a3a
Elie <elie.marouani@gmail.com>
2019-01-18 17:53:24 +0100

    ajout de la methode de heron

--- sqrt.py
+++ sqrt.py
@@ -7,3 +7,18 @@ Created on Fri Jan 18 11:12:32 2019
 
 def sqrt(x):
     return (round(x**0.5,3))
+
+def Heron(value):
+    if value < 0:
+        value = abs(value) #Value must stay positive
+    closeness = 0.01 #sharpness
+    guess = value / 3.0  #initial value of the suite
+    while(True):
+        to_find = (guess * guess) - value 
+        absTo_find = abs(to_find) #to_find must stay positive
+        if absTo_find <= closeness: 
+            return round(guess,3)
+        guess = (guess + (value/guess))/2.0  #main formula
+        
+print(Heron(15))
+        
